c38acb1698e576ff60d1b34f8119eaeda9be0e39
Keerthana V <153579116+Keerthana-v8@users.noreply.github.com>
2026-04-26 22:18:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Todo App 🚀");
+res.send("Hello Updated App 🚀");
 });
 
 app.listen(3000, () => {
